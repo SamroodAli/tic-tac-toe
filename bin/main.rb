a9290b60d07ad83e1d995c7f3e95173a7cc5739e
Marijan Brvar <marijan.brvar@gmail.com>
2021-03-25 15:12:28 +0100

    Create 3x3 board with 1-9 values close #6

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
+board = %w[1 2 3 4 5 6 7 8 9]
 
-puts 'Hello world!'
+def display_board(board)
+  puts '+---+---+---+'
+  puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+  puts '+---+---+---+'
+  puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+  puts '+---+---+---+'
+  puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+  puts '+---+---+---+'
+end
+
+puts display_board(board)
