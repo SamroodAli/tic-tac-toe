1aa5f590a322f164442260579df7d535d478bf34
samroodAli <devsamrood@gmail.com>
2021-03-25 19:52:12 +0530

    prompt users to enter names and assign symbols to players

--- bin/main.rb
+++ bin/main.rb
@@ -11,4 +11,28 @@ def display_board(board)
   puts '+---+---+---+'
 end
 
-puts display_board(board)
+puts 'Lets play some Tic Tac Toe'
+puts
+puts '.::First Player::.Please Enter Your Name: '
+player1 = gets.chomp.capitalize
+puts "Welcome #{player1} your symbol is X "
+puts
+puts '.::Second Player::. Please Enter Your Name: '
+player2 = gets.chomp.capitalize
+puts "Welcome #{player2} your symbol is O "
+puts
+puts
+puts "#{player1} vs #{player2}"
+display_board(board)
+puts
+4.times do
+  puts "It is your turn #{player1}, position your Symbol on board"
+  move = gets.chomp.to_i
+  board[move] = 'X'
+  display_board(board)
+  puts "It is your turn #{player2}, position your Symbol on board"
+  move = gets.chomp.to_i
+  board[move] = 'O'
+  display_board(board)
+end
+
