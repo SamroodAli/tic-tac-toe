c2b7420f0be71c2bd15f2a13e94f739e8c45b1a0
samroodAli <devsamrood@gmail.com>
2021-03-29 14:17:19 +0530

    create board class with update method

---
+++ lib/board.rb
@@ -0,0 +1,32 @@
+class Board
+  attr_accessor :board
+
+  WIN_SEQUENCE = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+  ]
+
+  def initialize()
+    @board = (1..9).to_a
+  end
+
+  def tie?
+    @board.all? { |e| e.is_a?(String) }
+  end
+
+  def win?
+    WIN_SEQUENCE.any? do |combination|
+      combination.all? { |idx| @board[idx] == 'X' } || combination.all? { |idx| @board[idx] == 'O' }
+    end
+  end
+
+  def update_board(current_player, position, player1, player2)
+    @board[position - 1] = current_player == player1 ? player1.symbol : player2.symbol
+  end
+end
