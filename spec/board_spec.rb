dc6f6d62a0e0b57f11db4b974722beb30f9f2b16
samroodAli <devsamrood@gmail.com>
2021-04-01 10:42:50 +0530

    Gemfile and Gemfile lock for rspec gem

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,5 +2,11 @@ require "board"
 
 describe "Board" do
   subject(:board) {Board.new}
+  describe "#initialize" do
+    it "should set instance variable @board" do
+      expect(:board).instance_variable_get(:@board)
+      
+    end
+  end
 
 end
