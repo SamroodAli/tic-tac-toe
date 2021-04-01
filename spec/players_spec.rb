81d352d44610a3568726e18d9cd0e9c4f72e28b0
samroodAli <devsamrood@gmail.com>
2021-04-01 12:12:16 +0530

    contexts arguments given and not in player rspec

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -1,9 +1,17 @@
 require 'players'
 
 describe 'Player' do
-  subject(:player1) { Player.new }
+  subject(:player1) { Player.new("Samrood","X") }
+  context "when no arguments are given" do
+    it "should raise argument error" do
+      expect{Player.new}.to raise_error ArgumentError
+    end
+  end
 
-  describe
-
+  context "when arguments are given" do
+    it "should raise not argument error" do
+      expect{player1}.not_to raise_error ArgumentError
+    end
+  end
 
 end
