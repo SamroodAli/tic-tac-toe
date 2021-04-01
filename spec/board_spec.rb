2a41b452d897b84820b0a5db6ef0654c5b4f43e1
samroodAli <devsamrood@gmail.com>
2021-04-01 11:53:31 +0530

    contexts in update board for arugments given and not

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,8 @@ require 'board'
 
 describe 'Board' do
   subject(:game_board) { Board.new }
-  let(:player1){double("player",:symbol => "X")}
-  let(:player2){double("player",:symbol => "O")}
+  let(:player1) { double('player', symbol: 'X') }
+  let(:player2) { double('player', symbol: 'O') }
 
   describe '#initialize' do
     it 'should initialize instance variable @board to array with with numbers from 1 to 9' do
@@ -30,50 +30,60 @@ describe 'Board' do
     end
   end
 
-  describe "#tie?" do
-
-    it "should return false before game" do
+  describe '#tie?' do
+    it 'should return false before game' do
       expect(game_board.tie?).to be false
     end
 
-    it "should return true if all the slots are filled with strings" do
+    it 'should return true if all the slots are filled with strings' do
       tie = %w[X O X O X O X O O]
-      game_board.instance_variable_set(:@board,tie)
+      game_board.instance_variable_set(:@board, tie)
       expect(game_board.tie?).to be true
     end
-    it "should return false if there are any slots left(integers left)" do
-      current_board = ["X",2,"O","X",5,6,7,8,9]
-      game_board.instance_variable_set(:@board,current_board)
+    it 'should return false if there are any slots left(integers left)' do
+      current_board = ['X', 2, 'O', 'X', 5, 6, 7, 8, 9]
+      game_board.instance_variable_set(:@board, current_board)
       expect(game_board.tie?).to be false
     end
   end
 
-  describe "#win?" do
-    it "should return false before game" do
+  describe '#win?' do
+    it 'should return false before game' do
       expect(game_board.win?).to be false
     end
 
-    it "should return true if there is a winning combination" do
-      winning_combination = [1,"X","O","O","X","O",7,"X","O"]
-       game_board.instance_variable_set(:@board,winning_combination)
-       expect(game_board.win?).to be true
+    it 'should return true if there is a winning combination' do
+      winning_combination = [1, 'X', 'O', 'O', 'X', 'O', 7, 'X', 'O']
+      game_board.instance_variable_set(:@board, winning_combination)
+      expect(game_board.win?).to be true
     end
 
-    it "should return false if there are no winning conditions" do
+    it 'should return false if there are no winning conditions' do
       no_win_sequence = %w[X O X O 5 O X 8 O]
-      game_board.instance_variable_set(:@board,no_win_sequence)
+      game_board.instance_variable_set(:@board, no_win_sequence)
       expect(game_board.win?).to be false
     end
   end
 
-  describe "#update_board" do
-    it "should take in current player as Player, position as integer, two Player instances" do
-      expect{game_board.update_board}.to raise_error ArgumentError
+  describe '#update_board' do
+    context 'when arguments are not given' do
+      it 'should take in current player as Player, position as integer, two Player instances' do
+        expect { game_board.update_board }.to raise_error ArgumentError
+      end
     end
 
-    it "should set a marker on position for current player" do
-      current_player = player1
+    context 'when arguments are given' do
+      it 'should update board with marker on index (position-1) given for current player after invoking update_board' do
+        current_player = player1
+        game_board.update_board(current_player, 1, player1, player2)
+        expect(game_board.board[0]).to eq('X')
+      end
+
+      it 'should not update position, but position - 1' do
+        current_player = player1
+        game_board.update_board(current_player, 1, player1, player2)
+        expect(game_board.board[0]).to eq('X')
+      end
     end
   end
-
 end
